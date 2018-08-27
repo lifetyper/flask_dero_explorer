22cb02a7ca48966f2de38b1be94fb58af0acce07
lifetyper <leavic@gmail.com>
2018-08-27 10:53:49 +0800

    add uwsgi service config and nginx config

--- src/manage.py
+++ src/manage.py
@@ -4,7 +4,6 @@ from app import create_app
 
 app = create_app()
 
-app.run(host='0.0.0.0')
-
-# app.run(host='0.0.0.0', port=443,
-#         ssl_context=('/etc/letsencrypt/live/dero.cash/fullchain.pem', '/etc/letsencrypt/live/dero.cash/privkey.pem'))
+app.run(host='0.0.0.0',port=8443)
+#app.run(host='0.0.0.0', port=8443,
+#       ssl_context=('/etc/letsencrypt/live/dero.cash/fullchain.pem', '/etc/letsencrypt/live/dero.cash/privkey.pem'))
--- src/uwsgi.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-uwsgi -s /tmp/dexp.sock --manage-script-name --mount /dexp=manage:app --process 5
